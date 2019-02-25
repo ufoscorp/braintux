24cc7c8baa4fcaa7e78859806cb61fc8a8e9510b
Davi William Moraes Suga <33470530+daviwms999@users.noreply.github.com>
2019-02-25 11:34:34 -0300

    Update braintux-installer-linux.sh

--- braintux-installer-linux.sh
+++ braintux-installer-linux.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 path = $(pwd)
-
-cp -r $pwd /opt
-
+mkdir /opt/braintux
+cp -r $pwd /opt/
 echo "alias braintux='/usr/bin/env python3 /opt/braintux/braintux-core.py'" >> ~/.bashrc
-
 env bash -l

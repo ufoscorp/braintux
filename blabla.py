7d6b86fffc4b2ed8f27560104420e40592878974
cl3t0 <cl3t0.sh@gmail.com>
2019-03-03 17:18:34 -0300

    Deleting some trash files

--- blabla.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-p= subprocess.Popen("python3 /opt/braintux-master/braintux-core.py ping terminal", stdout=subprocess.PIPE,shell=True)
-
-output = p.communicate()[0].decode('utf-8')
-
-print("saida do comando:",output=="On\n")
